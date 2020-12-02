428279a32c6211c2efca5ac058cae82af5c53a36
Alejandro Mendoza <alemendoza0197@gmail.com>
2020-12-02 14:45:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## LinkinApps microfrontend webpack
---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+// MOUNT FUNCTION TO START UP THE APP
+const mount = (el) => {
+  ReactDom.render( <App />, el );
+};
+
+// IF WE ARE IN DEVELOPMENT AND IN ISOLATION,
+// CALL MOUNT INMEDIATELY
+if(process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot){
+    mount(devRoot);
+  }
+}
+
+// WE ARE RUNNING THROUGH CONTAINER
+// AND WE SHOULD EXPORT MOUNT FUNCTION
+
+export { mount };
