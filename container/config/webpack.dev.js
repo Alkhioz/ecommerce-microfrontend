2bb9573a242144a957fc17ddb30ada85670b4e60
Alejandro Mendoza <alemendoza0197@gmail.com>
2020-12-03 12:53:00 -0500

    configure container app

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,23 +1,21 @@
 const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
 const commonConfig = require('./webpack.common')
 
 const devConfig = {
   mode: 'development',
   devServer: {
-    port: 8081,
+    port: 8080,
     historyApiFallback: {
       index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'marketing',
-      filename: 'remoteEntry.js',
-      exposes: {
-	'./MarketingApp': './src/bootstrap',
+      name: 'container',
+      remotes: {
+	marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
     }),
     new HtmlWebpackPlugin ({
