c975cc912bea04f2a903bc3c715dbaefb4ad4602
Alejandro Mendoza <alemendoza0197@gmail.com>
2020-12-03 15:56:03 -0500

    letting webpack access dependencies from packagesjson for shared dependencies

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,8 +1,8 @@
 const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
-const commonConfig = require('./webpack.common')
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -19,8 +19,8 @@ const devConfig = {
       exposes: {
 	'./MarketingApp': './src/bootstrap',
       },
-      shared: ['react', 'react-dom'],
-    }),
+      shared: packageJson.dependencies, 
+     }),
     new HtmlWebpackPlugin ({
       template: './public/index.html',
     }),
